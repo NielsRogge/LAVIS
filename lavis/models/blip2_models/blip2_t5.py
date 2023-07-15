Fix precision to float32

lavis/models/blip2_models/blip2_t5.py
@@ -85,9 +85,9 @@ class Blip2T5(Blip2Base):
             t5_model, config=t5_config
         )
 
-        for name, param in self.t5_model.named_parameters():
-            param.requires_grad = False
-            param.data = param.data.bfloat16()
+        # for name, param in self.t5_model.named_parameters():
+        #     param.requires_grad = False
+        #     param.data = param.data.bfloat16()
 
         self.t5_proj = nn.Linear(
             self.Qformer.config.hidden_size, self.t5_model.config.hidden_size
@@ -119,41 +119,42 @@ class Blip2T5(Blip2Base):
         inputs_t5 = self.t5_proj(query_output.last_hidden_state)
         atts_t5 = torch.ones(inputs_t5.size()[:-1], dtype=torch.long).to(image.device)
 
-        with self.maybe_autocast(dtype=torch.bfloat16):
-            input_tokens = self.t5_tokenizer(
-                samples["text_input"],
-                padding="longest",
-                truncation=True,
-                max_length=self.max_txt_len,
-                return_tensors="pt",
-            ).to(image.device)
-            output_tokens = self.t5_tokenizer(
-                samples["text_output"],
-                padding="longest",
-                truncation=True,
-                max_length=self.max_txt_len,
-                return_tensors="pt",
-            ).to(image.device)
+        # with self.maybe_autocast(dtype=torch.bfloat16):
+        input_tokens = self.t5_tokenizer(
+            samples["text_input"],
+            padding="longest",
+            truncation=True,
+            max_length=self.max_txt_len,
+            return_tensors="pt",
+        ).to(image.device)
+        output_tokens = self.t5_tokenizer(
+            samples["text_output"],
+            padding="longest",
+            truncation=True,
+            max_length=self.max_txt_len,
+            return_tensors="pt",
+        ).to(image.device)
 
-            encoder_atts = torch.cat([atts_t5, input_tokens.attention_mask], dim=1)
+        encoder_atts = torch.cat([atts_t5, input_tokens.attention_mask], dim=1)
 
-            targets = output_tokens.input_ids.masked_fill(
-                output_tokens.input_ids == self.t5_tokenizer.pad_token_id, -100
-            )
+        targets = output_tokens.input_ids.masked_fill(
+            output_tokens.input_ids == self.t5_tokenizer.pad_token_id, -100
+        )
 
-            inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
-            inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
+        inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
+        inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
 
-            outputs = self.t5_model(
-                inputs_embeds=inputs_embeds,
-                attention_mask=encoder_atts,
-                decoder_attention_mask=output_tokens.attention_mask,
-                return_dict=True,
-                labels=targets,
-            )
-            loss = outputs.loss
+        outputs = self.t5_model(
+            inputs_embeds=inputs_embeds,
+            attention_mask=encoder_atts,
+            decoder_attention_mask=output_tokens.attention_mask,
+            return_dict=True,
+            labels=targets,
+        )
+        return outputs
+        # loss = outputs.loss
 
-            return {"loss": loss}
+        # return {"loss": loss}
 
     @torch.no_grad()
     def generate(
@@ -221,26 +222,26 @@ class Blip2T5(Blip2Base):
 
         encoder_atts = torch.cat([atts_t5, input_tokens.attention_mask], dim=1)
 
-        with self.maybe_autocast(dtype=torch.bfloat16):
-            inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
-            inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
+        # with self.maybe_autocast(dtype=torch.bfloat16):
+        inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
+        inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
 
-            outputs = self.t5_model.generate(
-                inputs_embeds=inputs_embeds,
-                attention_mask=encoder_atts,
-                do_sample=use_nucleus_sampling,
-                top_p=top_p,
-                temperature=temperature,
-                num_beams=num_beams,
-                max_new_tokens=max_length,
-                min_length=min_length,
-                repetition_penalty=repetition_penalty,
-                length_penalty=length_penalty,
-                num_return_sequences=num_captions,
-            )
-            output_text = self.t5_tokenizer.batch_decode(
-                outputs, skip_special_tokens=True
-            )
+        outputs = self.t5_model.generate(
+            inputs_embeds=inputs_embeds,
+            attention_mask=encoder_atts,
+            do_sample=use_nucleus_sampling,
+            top_p=top_p,
+            temperature=temperature,
+            num_beams=num_beams,
+            max_new_tokens=max_length,
+            min_length=min_length,
+            repetition_penalty=repetition_penalty,
+            length_penalty=length_penalty,
+            num_return_sequences=num_captions,
+        )
+        output_text = self.t5_tokenizer.batch_decode(
+            outputs, skip_special_tokens=True
+        )
 
         return output_text
 
@@ -289,22 +290,22 @@ class Blip2T5(Blip2Base):
 
         encoder_atts = torch.cat([atts_t5, input_tokens.attention_mask], dim=1)
 
-        with self.maybe_autocast(dtype=torch.bfloat16):
-            inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
-            inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
+        # with self.maybe_autocast(dtype=torch.bfloat16):
+        inputs_embeds = self.t5_model.encoder.embed_tokens(input_tokens.input_ids)
+        inputs_embeds = torch.cat([inputs_t5, inputs_embeds], dim=1)
 
-            outputs = self.t5_model.generate(
-                inputs_embeds=inputs_embeds,
-                attention_mask=encoder_atts,
-                do_sample=False,
-                num_beams=num_beams,
-                max_new_tokens=max_len,
-                min_length=min_len,
-                length_penalty=length_penalty,
-            )
-            output_text = self.t5_tokenizer.batch_decode(
-                outputs, skip_special_tokens=True
-            )
+        outputs = self.t5_model.generate(
+            inputs_embeds=inputs_embeds,
+            attention_mask=encoder_atts,
+            do_sample=False,
+            num_beams=num_beams,
+            max_new_tokens=max_len,
+            min_length=min_len,
+            length_penalty=length_penalty,
+        )
+        output_text = self.t5_tokenizer.batch_decode(
+            outputs, skip_special_tokens=True
+        )
 
         if self._apply_lemmatizer:
             output_text = self._lemmatize(output_text)